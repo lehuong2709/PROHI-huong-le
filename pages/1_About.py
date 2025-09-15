b8a9a57af8c23e4468bdeda869ae670d04bd855c
lehuong2709 <huonglt.hup@gmail.com>
2025-09-15 13:43:17 +0200

    About page formatting
    Format text in page

--- pages/1_About.py
+++ pages/1_About.py
@@ -2,11 +2,21 @@ import streamlit as st
 
 st.set_page_config(page_title="About", page_icon="📝")
 
-st.title("Huong Le — About")
+# Title
+st.markdown(
+    "<h1 style='font-size: 36px;'>Huong Le — About</h1>",
+    unsafe_allow_html=True
+)
+
+# Subtitle
+st.markdown(
+    "<h2 style='font-size: 24px;'>Project Summary (DSHI Course)</h2>",
+    unsafe_allow_html=True
+)
 
 st.markdown(
 """
-## Project Summary (DSHI Course)
+<div style="text-align: justify; font-size: 18px; line-height: 1.6;">
 
 In my project last semester, I worked with a **Sleep Disorder dataset** to practice building
 a classification model. The main idea was to predict whether a patient might have a sleep
@@ -18,5 +28,5 @@ Among the models I tried, a **Decision Tree (DT-3)** had the best performance ma
 
 Since this dataset was synthetic, I decided not using the model directly in clinical practice.
 For me, the results are more of a **proof of concept**. 
-The model neeed to trained and validated on actual patient data"""
+The model neeed to trained and validated on actual patient data."""
 )
